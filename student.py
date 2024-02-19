5d8e351059e2ce946afc8fcaefeecca736b66cbf
clever.mbava84@yahoo.com <clevermbava84@yahoo.com>
2024-02-19 20:09:28 +0200

    added method to find highest grade

--- student.py
+++ student.py
@@ -12,11 +12,22 @@ class Student():
         average = sum(self.grades)/len(self.grades)
         print("The average for student " + self.name + " is " + str(average))
 
+ #  Method to calculate student highest grade
+    def highest_student_grade(self):
+        highest_grade = max(self.grades)
+        print("The highest grade for student " + self.name + " is " + str(highest_grade))
+
 # Create Student objects
 philani  = Student(20, "Philani Sithole", "Male", [64,65])
 sarah = Student(19, "Sarah Jones", "Female", [82,58])
+peter= Student(21, "Peter Moyo", "Male", [76,90])
+claire = Student(19, "Claire Brooks", "Female", [45,80])
+kelly = Student(20, "Kelly Rodgers", "Female", [89,97])
+
 
 # Method call
-sarah.compute_average()
+claire.compute_average()
 
 # Add in new objects and logic here, and above to test your understanding.
+#Call method to find highest grade for student
+peter.highest_student_grade()
